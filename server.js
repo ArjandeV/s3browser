713ec14693fa328e19091741a6af941ec071cfc9
mrzapp <contact@jeppezapp.com>
2016-01-06 22:28:59 +0100

    subdirs

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// ----------
+// Preload
+// ----------
 // Load the plugin modules
 let express = require('express');
 let path = require('path');
@@ -16,34 +19,46 @@ let app = express();
 
 app.set('view engine', 'jade');
 
-app.use(express.static(__dirname + '/public'));
-
+// ----------
+// Routes
+// ----------
+// Redirect root to browsing route
 app.get('/', function(req, res) {
     res.redirect('/browse/');
 });
 
+// Browsing route for navigating file tree
 app.get('/browse/*', function(req, res) {
     let url = decodeURI(req.url.replace('/browse/', ''));
     let params = {
         s3Params: {
             Bucket: env.bucket,
+            Delimiter: '/',
             Prefix: url
         } 
     };
     
     let emitter = client.listObjects(params);
+    let sent = false;
 
     emitter.on('data', function(data) {
-        res.render('index', {
-            req: req,
-            path: path,
-            dirname: url,
-            contents: data.Contents
-        });
+        if(!sent) {
+            sent = true;
+
+            res.render('index', {
+                req: req,
+                path: path,
+                dirname: url,
+                commonPrefixes: data.CommonPrefixes || [],
+                contents: data.Contents || [],
+                bucket: env.bucket
+            });
+        }
     });
 
 });
 
+// Streaming route for passing through file data
 app.get('/stream/*', function(req, res) {
     let url = decodeURI(req.url.replace('/stream/', ''));
     let params = {
@@ -56,5 +71,6 @@ app.get('/stream/*', function(req, res) {
     stream.pipe(res);
 });
 
-
 let server = app.listen(8000);
+
+console.log('s3browser is running on port 8000');
